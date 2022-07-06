630711b5144b9014078948c53248964390aafb46
William <william.n.vanbuskirk@gmail.com>
2022-07-06 12:06:50 -0500

    added deep learning feature

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 #importing the necessary libraries for deployment
 from flask import Flask, jsonify, request, render_template
-import urllib.request
-import json
 import os
-import ssl
-from numpy import diag
-ML_APP_KEY = os.environ['ML_APP_KEY']
-TEST_ENV = os.environ['WEBSITE_SITE_NAME']
+import pickle
+from ml_api import predict_ml, predict_tf
 
-def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
-    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-        ssl._create_default_https_context = ssl._create_unverified_context
 
-#allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+ML_APP_KEY = os.getenv('ML_APP_KEY')
+TEST_ENV = os.getenv('WEBSITE_SITE_NAME')
+ml_url = 'http://3cbc4439-5001-4b6d-b704-adbfd1c79dd0.eastus.azurecontainer.io/score'
+
 
 
 #naming our app as app
@@ -23,63 +18,43 @@ app= Flask(__name__)
 
 @app.route("/")
 def home():
-    print(TEST_ENV)
-    print(ML_APP_KEY)
     return render_template("index.html")
+
+@app.route("/jupyter")
+def jupyter():
+    return render_template("jupyter.html")
+
+@app.route("/deep_learning")
+def deep_learning_overview():
+    return render_template("deep-learning.html")
+
+@app.route("/data_context")
+def data_context():
+    return render_template("data_overview.html")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     form_input = dict(request.form)
-    # Request data goes here
-    # The example below assumes JSON formatting which may be updated
-    # depending on the format your endpoint expects.
-    # More information can be found here:
-    # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
-    data =  {
-    "Inputs": {
-        "data": [
-        {
-            "pelvic_incidence": form_input['pelvic_incidence'],
-            "pelvic_tilt": form_input['pelvic_tilt'],
-            "lumbar_lordosis_angle": form_input['lumbar_lordosis_angle'],
-            "sacral_slope": form_input['sacral_slope'],
-            "pelvic_radius": form_input['pelvic_radius'],
-            "degree_spondylolisthesis": form_input['degree_spondylolisthesis']
-        }
-        ]
-    },
-    "GlobalParameters": {
-        "method": "predict"
-    }
-    }
-
-    body = str.encode(json.dumps(data))
-
-    url = 'http://3cbc4439-5001-4b6d-b704-adbfd1c79dd0.eastus.azurecontainer.io/score'
-    api_key = ML_APP_KEY # Replace this with the API key for the web service
-
-    # The azureml-model-deployment header will force the request to go to a specific deployment.
-    # Remove this header to have the request observe the endpoint traffic rules
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
-
-    req = urllib.request.Request(url, body, headers)
-
-    try:
-        response = urllib.request.urlopen(req)
-
-        result = response.read()
-        #print(result)
-        result_dict = json.loads(result.decode('utf-8'))
-        diagnosis = (result_dict['Results'][0])
-        #print(diagnosis)
-    except urllib.error.HTTPError as error:
-        print("The request failed with status code: " + str(error.code))
-
-        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-        print(error.info())
-        print(error.read().decode("utf8", 'ignore'))
+    diagnosis = predict_ml(ortho_dict = form_input, url = ml_url, api_key =  ML_APP_KEY)
     return render_template("index.html", prediction_text= "Diagnosis is {}".format(diagnosis))
+
+@app.route('/predict_deep')
+def deep_learning_pred():
+    return render_template("deep-learning-pred.html")
+
+@app.route('/predict_deep_output',methods=['POST'])
+def deep_learning_pred_output():
+    diagnosis = predict_tf(request)
+    return render_template("deep-learning-pred.html", prediction_text= "Diagnosis is {}".format(diagnosis))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ ml_api.py
@@ -0,0 +1,76 @@
+import urllib.request
+import json
+import ssl
+import os
+import tensorflow as tf
+import numpy as np
+tf.get_logger().setLevel('FATAL')
+
+from sympy import diag
+
+def predict_ml(ortho_dict, url, api_key):
+    # Request data goes here
+    # The example below assumes JSON formatting which may be updated
+    # depending on the format your endpoint expects.
+    # More information can be found here:
+    # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+    def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+        if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+            ssl._create_default_https_context = ssl._create_unverified_context
+    #allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+    data =  {
+    "Inputs": {
+        "data": [
+        {
+            "pelvic_incidence": ortho_dict['pelvic_incidence'],
+            "pelvic_tilt": ortho_dict['pelvic_tilt'],
+            "lumbar_lordosis_angle": ortho_dict['lumbar_lordosis_angle'],
+            "sacral_slope": ortho_dict['sacral_slope'],
+            "pelvic_radius": ortho_dict['pelvic_radius'],
+            "degree_spondylolisthesis": ortho_dict['degree_spondylolisthesis']
+        }
+        ]
+    },
+    "GlobalParameters": {
+        "method": "predict"
+    }
+    }
+
+    body = str.encode(json.dumps(data))
+   
+    # The azureml-model-deployment header will force the request to go to a specific deployment.
+    # Remove this header to have the request observe the endpoint traffic rules
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+
+        result = response.read()
+        #print(result)
+        result_dict = json.loads(result.decode('utf-8'))
+        diagnosis = (result_dict['Results'][0])
+        #print(diagnosis)
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
+    return diagnosis
+
+
+def predict_tf(request, dir='static/models/tf_model'):
+    deep_model = tf.keras.models.load_model(dir)
+    decoder = {'Normal': 0, 'Hernia':1, 'Spondylolisthesis':2 }
+    int_features = [float(x) for x in request.form.values()]
+    print(int_features)
+    tf_feature = tf.convert_to_tensor([np.array(int_features)])
+    print(tf_feature)
+    prediction = deep_model.predict(tf_feature)
+    print(prediction)
+    value = np.argmax(prediction, axis=1)
+    diagnosis = list(decoder.keys())[list(decoder.values()).index(value)]
+    return diagnosis
